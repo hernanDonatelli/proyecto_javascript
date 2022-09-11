c9cf449503d33406c7ba48e99d5a972f7055ab3e
Hernan Donatelli <hernandonatelli@gmail.com>
2022-09-11 18:13:02 -0300

    borrado de codigo comentado en home.js

--- js/home.js
+++ js/home.js
@@ -24,36 +24,14 @@ for (const producto of productos) {
 
                         </div>`;
 
-    /* articulo.innerHTML = `<div class="brand brand-${producto.categoria}">
-                        <h2>${producto.marca}</h2>
-                    </div>
-
-                    <div class="shop__card__header">
-                        <img src="${producto.img}" alt="${producto.marca} - ${producto.nombre}">
-                    </div>
-
-                    <div class="shop__card__content">
-                        <div class="content__price">
-                            <span class="title">${producto.nombre}</span>
-                            <span class="price mt-3">$${producto.precio}</span>
-                        </div>
-                    </div>
-
-                    <div class="shop__card__actions my-3">
-                        <button id=btn-${producto.id} class="btn_shop_card">
-                            <iconify-icon class="market" icon="map:grocery-or-supermarket"></iconify-icon>
-                            <span>Agregar al carrito</span>
-                        </button>
-                    </div>`; */
-
     container.append(articulo);
-/*
+
     //Eventos
     const boton = document.getElementById(`btn-${producto.id}`);
 
     boton.addEventListener('click', () => {
         alert(`Has agregado ${producto.nombre}`);
     })
- */
+
 }
 
